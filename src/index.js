f07641dda2573d78225dbf94cf9dd44ada805307
rexgarland <rex.garland@gmail.com>
2021-08-08 21:27:54 -0500

    basic functionality, visual tuning needed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const data = require('./json/data.json');
 
 const ui = {
   plot: document.getElementById('plot'),
-  year: document.getElementById('year-input')
+  yearInput: document.getElementById('year-input'),
+  yearDisplay: document.getElementById('year-display'),
 };
 
 const svg = require('./svg');
@@ -24,8 +25,8 @@ const tickSize = {
   minor: 6
 }
 const textSize = {
-  height: 35,
-  emHeight: 26,
+  height: 37,
+  emHeight: 27,
   width: 16,
   offset: 10
 }
@@ -49,71 +50,119 @@ function year2Index(year) {
   return year-1990;
 }
 
-let year = 2000;
+const startingYear = 1990;
+
+function gridLine(v1, v2) {
+  return svg.createLine(v1, v2, plotConfig.grid.thickness, colors.grid.stroke);
+}
 
 function createPlot() {
 
-  // set up plot graphics
-  ui.plot.appendChild(plotLine(new Vec2(plotBox.x-tickSize.major, plotBox.y), new Vec2(plotBox.x+plotBox.width,plotBox.y)));
-  ui.plot.appendChild(plotLine(new Vec2(plotBox.x, plotBox.y+tickSize.major), new Vec2(plotBox.x, plotBox.y-plotBox.height)));
+  // set up plot
+
   // x axis
+  ui.plot.appendChild(gridLine(new Vec2(plotBox.x-tickSize.major, plotBox.y), new Vec2(plotBox.x+plotBox.width,plotBox.y)));
   range(0,plotConfig.x.grid.stop+1,plotConfig.x.grid.tick).forEach(i => {
     const start = plot2svg(new Vec2(i,0)).add(new Vec2(0,tickSize.major));
     const end = plot2svg(new Vec2(i,plotConfig.y.grid.stop));
-    ui.plot.appendChild(plotLine(start, end));
-    const t = svg.createText(start.x-textSize.width/2, start.y+textSize.height+textSize.offset, `${i}`);
+    ui.plot.appendChild(gridLine(start, end));
+    const t = svg.createText(start.x-textSize.width/2, start.y+textSize.emHeight+textSize.offset, `${i}`);
     t.className.baseVal = 'svg-axis-text';
     ui.plot.appendChild(t);
   })
-  
-  
+
   // y axis
+  ui.plot.appendChild(gridLine(new Vec2(plotBox.x, plotBox.y+tickSize.major), new Vec2(plotBox.x, plotBox.y-plotBox.height)));
   range(0,plotConfig.y.grid.stop+1,plotConfig.y.grid.tick).forEach(i => {
     const start = plot2svg(new Vec2(0,i)).add(new Vec2(-tickSize.major,0));
     const end = plot2svg(new Vec2(plotConfig.x.grid.stop,i));
-    ui.plot.appendChild(plotLine(start, end));
+    ui.plot.appendChild(gridLine(start, end));
     const text = `${i}`;
-    const t = svg.createText(start.x-textSize.width*text.length-textSize.offset, start.y+(textSize.emHeight-textSize.height)+textSize.height/2, text);
-    t.className.baseVal = 'svg-axis-text';
+    const t = svg.createText(start.x-textSize.width*text.length*1.2-textSize.offset, start.y+textSize.emHeight/2, text);
+    t.className.baseVal = 'svg-legend-text';
     ui.plot.appendChild(t);
   })
   if (plotConfig.y.grid.minor) {
     range(0,plotConfig.y.grid.stop+1,plotConfig.y.grid.minor).forEach(i => {
       const end = plot2svg(new Vec2(0,i));
       const start = end.add(new Vec2(-tickSize.minor,0));
-      ui.plot.appendChild(plotLine(start, end));
+      ui.plot.appendChild(gridLine(start, end));
     })
   }
 
   // plot legend
-  const bottomLeft = plot2svg(new Vec2(4,20));
-  const topRight = plot2svg(new Vec2(5,25));
+  const bottomLeft = plot2svg(new Vec2(5,15));
+  const topRight = plot2svg(new Vec2(6,20));
   const middleMiddle = bottomLeft.add(topRight.subtract(bottomLeft).multiply(0.5))
   ui.plot.appendChild(plotRect(bottomLeft.x,topRight.y,topRight.x-bottomLeft.x,bottomLeft.y-topRight.y,colors.legend.stroke,colors.legend.fill));
-  ui.plot.appendChild(svg.createText(middleMiddle.x,middleMiddle.y,plotConfig.legend))
+  const legendText = svg.createText(middleMiddle.x,middleMiddle.y+textSize.emHeight,plotConfig.legend);
+  legendText.className.baseVal = 'svg-legend-text';
+  ui.plot.appendChild(legendText);
+  const legendTextBox = legendText.getBBox();
+  legendText.setAttribute('x',legendTextBox.x-legendTextBox.width/2);
+  legendText.setAttribute('y',legendTextBox.y+textSize.emHeight/2);
 
-  // plot data
-  let x = 0;
-  const i = year2Index(year);
-  data.forEach(({region, emission, population}) => {
-    const e = emission[i]/plotConfig.y.scale;
-    const p = population[i]/plotConfig.x.scale;
-    // console.log(region,e/p,p);
-    const v1 = plot2svg(new Vec2(x,0));
-    const v2 = plot2svg(new Vec2(x+p,e/p));
-    ui.plot.appendChild(plotRect(v1.x, v2.y, v2.x-v1.x, v1.y-v2.y, colors[region].stroke, colors[region].fill));
-    x = x + p;
+  // set up data
+
+  const elements = data.map(({region})=> {
+    const rect = svg.createRect(0,0,0,0,colors[region].stroke, colors[region].fill);
+    const arrowHead = svg.createArrow(new Vec2(0,0), new Vec2(1,1), '');
+    const arrowLine = svg.createLine(new Vec2(0,0), new Vec2(1,1), plotConfig.arrow.thickness);
+    const arrow = {arrowHead, arrowLine};
+    const text = svg.createText(0,0,'');
+    [rect, arrowHead, arrowLine, text].forEach(e=>ui.plot.appendChild(e));
+    return {rect, arrow, text}
   });
+
+  return elements;
 }
 
 // create plot elements
-createPlot();
+ui.data = createPlot();
 
 function render(year) {
-  console.log(year);
+  const index = year2Index(year);
+  range(0,data.length).reduce((x,i) => {
+    const {rect, arrow, text} = ui.data[i];
+    const {arrowHead, arrowLine} = arrow;
+    // get data
+    const {region, emission, population} = data[i];
+    const e = emission[index]/plotConfig.y.scale;
+    const p = population[index]/plotConfig.x.scale;
+    const width = p;
+    const height = e/p;
+    // map to plot space
+    const bottomLeft = plot2svg(new Vec2(x,0));
+    const topRight = plot2svg(new Vec2(x+width,height));
+    // update box
+    svg.updateRect(bottomLeft.x, topRight.y, topRight.x-bottomLeft.x, bottomLeft.y-topRight.y, colors[region].stroke, plotConfig.box.thickness, colors[region].fill)(rect);
+    // update arrow
+    const arrowData = {
+      position: topRight
+        .add(new Vec2(1,-1).normalize().multiply(plotConfig.arrow.offset))
+        .add(new Vec2(1,-1).normalize().multiply(plotConfig.arrow.size)),
+      direction: new Vec2(-1,1),
+      size: plotConfig.arrow.size
+    };
+    svg.updateArrow(arrowData.position, arrowData.direction, arrowData.size)(arrowHead);
+    const p1 = arrowData.position;
+    const p2 = p1.add(new Vec2(1,-1).normalize().multiply(plotConfig.arrow.length));
+    svg.updateLine(p1, p2, plotConfig.arrow.thickness)(arrowLine);
+    // update text
+    const tStart = p2
+      .add(new Vec2(1,-1).normalize().multiply(plotConfig.arrow.text.offset))
+      .add(new Vec2(1,1).normalize().multiply(textSize.emHeight/2));
+    svg.updateText(tStart.x,tStart.y,region)(text);
+    text.className.baseVal = 'svg-region-text';
+    return x+width;
+  },0)
+  ui.yearDisplay.innerHTML = year;
 }
 
-ui.year.addEventListener('input', (e) => {
-  render(e.target.value);
+render(startingYear);
+
+ui.yearInput.addEventListener('input', (e) => {
+  const year = e.target.value;
+  render(year);
 });
 
--- src/svg.js
+++ src/svg.js
@@ -23,38 +23,66 @@ function createShape(data) {
   return shape;
 }
 
-function createRect(x,y,width,height,stroke,thickness,fill) {
+function updateRect(x,y,width,height,stroke,thickness,fill) {
+  return rect => {
+    rect.setAttribute('x', x);
+    rect.setAttribute('y', y);
+    rect.setAttribute('width', width);
+    rect.setAttribute('height', height);
+    rect.setAttribute('stroke', stroke);
+    rect.setAttribute('stroke-width', thickness);
+    rect.setAttribute('fill', fill);
+  }
+}
+
+function createRect(...args) {
   const rect = document.createElementNS("http://www.w3.org/2000/svg",'rect');
-  rect.setAttribute('x', x);
-  rect.setAttribute('y', y);
-  rect.setAttribute('width', width);
-  rect.setAttribute('height', height);
-  rect.setAttribute('stroke', stroke);
-  rect.setAttribute('stroke-width', thickness);
-  rect.setAttribute('fill', fill);
+  updateRect(...args)(rect);
   return rect;
 }
 
-function createLine(p1, p2, thickness=0.1, color="black") {
-  const data = `M ${p1.x} ${p1.y} L ${p2.x} ${p2.y}`;
-  return createPath(data, thickness, color);
+function updateLine(p1, p2, thickness=0.1, color="black") {
+  return (line) => {
+    const data = `M ${p1.x} ${p1.y} L ${p2.x} ${p2.y}`;
+    line.setAttribute('d', data);
+  }
 }
 
-function createText(x,y,text) {
+function createLine(p1, p2, thickness=0.1, color="black") {
+  const line = createPath('',thickness, color);
+  updateLine(p1, p2, thickness, color)(line);
+  return line;
+}
+
+function updateText(x,y,text) {
+  return textElem => {
+    textElem.setAttribute('x', x);
+    textElem.setAttribute('y', y);
+    textElem.innerHTML = text;
+  }
+}
+
+function createText(...args) {
   const textElem = document.createElementNS("http://www.w3.org/2000/svg",'text');
-  textElem.setAttribute('x', x);
-  textElem.setAttribute('y', y);
-  textElem.innerHTML = text;
+  updateText(...args)(textElem);
   return textElem;
 }
 
-function createArrow(position, direction, size) {
+function updateArrow(position, direction, size) {
   direction = direction.normalize();
   const perp = direction.rotate(90);
   const relPositions = [perp.multiply(-size/3), direction.multiply(size), perp.multiply(size/3)];
   const absPositions = relPositions.map(v=>position.add(v));
   const data = `M ${position.x} ${position.y} ${absPositions.map(v=>`L ${v.x} ${v.y}`).join(' ')} Z`;
-  return createShape(data, 0.05);
+  return (arrow) => {
+    arrow.setAttribute('d', data);
+  }
+}
+
+function createArrow(...args) {
+  const arrow = createShape('');
+  updateArrow(...args)(arrow);
+  return arrow;
 }
 
 function createPlot(points) {
@@ -66,8 +94,12 @@ function createPlot(points) {
 module.exports = {
   createPath,
   createLine,
+  updateLine,
   createShape,
   createArrow,
+  updateArrow,
   createRect,
-  createText
+  updateRect,
+  createText,
+  updateText
 }
