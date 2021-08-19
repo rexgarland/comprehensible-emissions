3241799ec9ed83441435de3eb2fd2e06dfb2a13b
rexgarland <rex.garland@gmail.com>
2021-08-19 14:09:36 -0500

    fixed y axis label

--- build_json.sh
+++ build_json.sh
@@ -1,4 +1,4 @@
-python scripts/process_emission.py
-python scripts/process_population.py
-python scripts/join_tables.py
-python scripts/render_json.py
+python3 scripts/process_emission.py
+python3 scripts/process_population.py
+python3 scripts/join_tables.py
+python3 scripts/render_json.py
